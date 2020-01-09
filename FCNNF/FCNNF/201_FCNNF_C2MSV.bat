caaca7b6a6750b8321830cba989677642a8b3e65
zhe <35060424+fzingithub@users.noreply.github.com>
2020-01-09 15:17:16 +0800

    201-add C2MSVL

---
+++ FCNNF/FCNNF/201_FCNNF_C2MSV.bat
@@ -0,0 +1,3 @@
+E:
+cd E:\000-常用\ICTT\工具\C2M转化器\C2MSVL
+C2MSVL.exe --keep --addr C:\Users\Administrator\Documents\GitHub\FCNNF_MC8.0_VS2015_WIN10\MC8.0\MC8.0 --otnm _MSVInput.m C:\Users\Administrator\Documents\GitHub\FCNNF_MC8.0_VS2015_WIN10\FCNNF\FCNNF\main.cpp
